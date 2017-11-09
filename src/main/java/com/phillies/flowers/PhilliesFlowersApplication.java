bacf2ccc773f7d17473c930949ba28a59faf7597
Robert Crowley <robert.crowley1995@gmail.com>
2017-11-09 15:29:32 +0000

    First Run
    9th November 2017

---
+++ src/main/java/com/phillies/flowers/PhilliesFlowersApplication.java
@@ -0,0 +1,14 @@
+package com.phillies.flowers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhilliesFlowersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhilliesFlowersApplication.class, args);
+	}
+	
+	
+}
