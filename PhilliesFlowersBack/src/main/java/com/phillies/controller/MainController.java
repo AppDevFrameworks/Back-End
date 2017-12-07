6a60ccede31d94c6efbf9ec6ba8b93f27a73307d
Robert Crowley <robert.crowley1995@gmail.com>
2017-12-07 14:07:03 +0000

    Added Controller to MainController

--- PhilliesFlowersBack/src/main/java/com/phillies/controller/MainController.java
+++ PhilliesFlowersBack/src/main/java/com/phillies/controller/MainController.java
@@ -9,6 +9,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
 public class MainController {
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String index(Model model) {
